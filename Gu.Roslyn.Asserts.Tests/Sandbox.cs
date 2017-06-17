44803e2156212db960a01320c80d716719503bdd
JohanLarsson <johan_gavle1@hotmail.com>
2017-06-17 18:59:43 +0200

    Update dependencies.

--- Gu.Roslyn.Asserts.Tests/Sandbox.cs
+++ Gu.Roslyn.Asserts.Tests/Sandbox.cs
@@ -9,17 +9,6 @@ namespace Gu.Roslyn.Asserts.Tests
     public class Sandbox
     {
         [Test]
-        public void ThrowAssertionExceptionUsingActivator()
-        {
-            var assemblyQualifiedName = typeof(AssertionException).AssemblyQualifiedName;
-            Console.WriteLine(assemblyQualifiedName);
-            var type = Type.GetType(
-                "NUnit.Framework.AssertionException, nunit.framework, Version=3.6.1.0, Culture=neutral, PublicKeyToken=2638cd05610744eb",
-                throwOnError: false);
-            Assert.Throws<NUnit.Framework.AssertionException>(() => throw (Exception)Activator.CreateInstance(type, "Error"));
-        }
-
-        [Test]
         public void FindReferences()
         {
             foreach (var assembly in typeof(Sandbox).Assembly.GetReferencedAssemblies())
