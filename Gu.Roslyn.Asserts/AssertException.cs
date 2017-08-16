0d93e636e9b891a8866cd584694ade8783313094
JohanLarsson <johan_gavle1@hotmail.com>
2017-08-16 21:47:22 +0200

    netstandard2.0

--- Gu.Roslyn.Asserts/AssertException.cs
+++ Gu.Roslyn.Asserts/AssertException.cs
@@ -46,16 +46,17 @@ namespace Gu.Roslyn.Asserts
         /// <summary>
         /// Create an <see cref="AssertException"/> or an exception specific to the currently used test framework if found.
         /// </summary>
-        /// <param name="innerException">The message explaining why the assertion failed.</param>
+        /// <param name="message">The message explaining why the assertion failed.</param>
+        /// <param name="innerException"> The exception that caused the current exception.</param>
         /// <returns>The exception.</returns>
-        public static Exception Create(Exception innerException)
+        public static Exception Create(string message, Exception innerException)
         {
             if (TryGetExceptionType(out Type type))
             {
-                return (Exception)Activator.CreateInstance(type, innerException.Message, innerException);
+                return (Exception)Activator.CreateInstance(type, message, innerException);
             }
 
-            return new AssertException(innerException.Message, innerException);
+            return new AssertException(message, innerException);
         }
 
         /// <inheritdoc />
