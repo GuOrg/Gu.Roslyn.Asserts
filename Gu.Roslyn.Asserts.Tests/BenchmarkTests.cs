21a64347e4b75d4f7f6f6370d38e9fc8fc0ef22b
JohanLarsson <johan_gavle1@hotmail.com>
2017-10-28 14:46:02 +0200

    More tests.

--- Gu.Roslyn.Asserts.Tests/BenchmarkTests.cs
+++ Gu.Roslyn.Asserts.Tests/BenchmarkTests.cs
@@ -1,8 +1,10 @@
 ﻿namespace Gu.Roslyn.Asserts.Tests
 {
+    using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
     using Microsoft.CodeAnalysis.CSharp.Syntax;
     using NUnit.Framework;
 
@@ -36,6 +38,44 @@
             Assert.AreSame(analyzer, benchmark.Analyzer);
             benchmark.Run();
             benchmark.Run();
+        }
+
+        [Test]
+        public async Task ClassLibrary1FieldNameMustNotBeginWithUnderscore()
+        {
+            var analyzer = new FieldNameMustNotBeginWithUnderscore();
+            var sln = CodeFactory.CreateSolution(
+                CodeFactory.FindProjectFile("ClassLibrary1.csproj"),
+                MetadataReferences.Transitive(typeof(Benchmark).Assembly).ToArray());
+            var benchmark = await Benchmark.CreateAsync(sln.Projects.Single(), analyzer).ConfigureAwait(false);
+            var expected = new[] { "private int _value;" };
+            CollectionAssert.AreEqual(expected, benchmark.SyntaxNodeActions.Select(x => x.Context.Node.ToString()));
+
+            expected = new[] { "ClassLibrary1.ClassLibrary1Class1._value" };
+            CollectionAssert.AreEqual(expected, benchmark.SyntaxNodeActions.Select(x => x.Context.ContainingSymbol.ToString()));
+            Assert.AreSame(analyzer, benchmark.Analyzer);
+            benchmark.Run();
+            benchmark.Run();
+        }
+
+        [Test]
+        public async Task ClassLibrary1FieldDeclarations()
+        {
+            var analyzer = new SyntaxNodeAnalyzer(SyntaxKind.FieldDeclaration);
+            var sln = CodeFactory.CreateSolution(
+                CodeFactory.FindProjectFile("ClassLibrary1.csproj"),
+                MetadataReferences.Transitive(typeof(Benchmark).Assembly).ToArray());
+            var benchmark = await Benchmark.CreateAsync(sln.Projects.Single(), analyzer).ConfigureAwait(false);
+            var expected = new List<string> { "private int _value;" };
+            CollectionAssert.AreEqual(expected, benchmark.SyntaxNodeActions.Select(x => x.Context.Node.ToString()));
+            CollectionAssert.IsEmpty(analyzer.Contexts);
+
+            benchmark.Run();
+            CollectionAssert.AreEqual(expected, analyzer.Contexts.Select(x => x.Node.ToString()));
+
+            expected.AddRange(expected);
+            benchmark.Run();
+            CollectionAssert.AreEqual(expected, analyzer.Contexts.Select(x => x.Node.ToString()));
         }
     }
 }
