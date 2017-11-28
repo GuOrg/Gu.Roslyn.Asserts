3c57675f8c44de090b9505aea4fcd37c903555bb
JohanLarsson <johan_gavle1@hotmail.com>
2017-11-28 10:47:39 +0100

    FieldNameMustNotBeginWithUnderscoreTwoDiagnostics
    (cherry picked from commit 49c8f6b507317019d6e72196aea13a322ac517a3)

---
+++ Gu.Roslyn.Asserts.Tests/Analyzers/FieldNameMustNotBeginWithUnderscoreTwoDiagnostics.cs
@@ -0,0 +1,83 @@
+namespace Gu.Roslyn.Asserts.Tests
+{
+    using System;
+    using System.Collections.Immutable;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.CSharp.Syntax;
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    internal class FieldNameMustNotBeginWithUnderscoreTwoDiagnostics : DiagnosticAnalyzer
+    {
+        public const string Id1 = "ID1";
+        public const string Id2 = "ID2";
+
+        private static readonly DiagnosticDescriptor Descriptor1 = new DiagnosticDescriptor(
+            Id1,
+            "Field names must not begin with underscore",
+            "Field '{0}' must not begin with an underscore",
+            "Naming",
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true);
+
+        private static readonly DiagnosticDescriptor Descriptor2 = new DiagnosticDescriptor(
+            Id2,
+            "Field names must not begin with underscore",
+            "Field '{0}' must not begin with an underscore",
+            "Naming",
+            DiagnosticSeverity.Warning,
+            isEnabledByDefault: true);
+
+        private static readonly Action<SyntaxNodeAnalysisContext> FieldDeclarationAction = HandleFieldDeclaration;
+
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Descriptor1, Descriptor2);
+
+        /// <inheritdoc/>
+        public override void Initialize(AnalysisContext context)
+        {
+            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.None);
+            context.EnableConcurrentExecution();
+            context.RegisterSyntaxNodeAction(FieldDeclarationAction, SyntaxKind.FieldDeclaration);
+        }
+
+        private static void HandleFieldDeclaration(SyntaxNodeAnalysisContext context)
+        {
+            var syntax = (FieldDeclarationSyntax)context.Node;
+            var variables = syntax.Declaration?.Variables;
+            if (variables == null)
+            {
+                return;
+            }
+
+            foreach (VariableDeclaratorSyntax variableDeclarator in variables.Value)
+            {
+                if (variableDeclarator == null)
+                {
+                    continue;
+                }
+
+                var identifier = variableDeclarator.Identifier;
+                if (identifier.IsMissing)
+                {
+                    continue;
+                }
+
+                if (!identifier.ValueText.StartsWith("_", StringComparison.Ordinal))
+                {
+                    continue;
+                }
+
+                var name = identifier.ValueText;
+                if (context.ContainingSymbol.DeclaredAccessibility == Accessibility.Public)
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Descriptor1, identifier.GetLocation(), name));
+                }
+                else
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Descriptor2, identifier.GetLocation(), name));
+                }
+            }
+        }
+    }
+}
