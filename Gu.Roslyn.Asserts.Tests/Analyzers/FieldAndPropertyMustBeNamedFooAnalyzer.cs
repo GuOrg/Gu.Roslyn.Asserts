606e5979444c2ccbae6c972596acd0976038c110
JohanLarsson <johan_gavle1@hotmail.com>
2017-12-01 18:31:20 +0100

    FieldAndPropertyMustBeNamedFooAnalyzer and fix.
    (cherry picked from commit 312eda4caf830059248733bc000fe6fc504359aa)

---
+++ Gu.Roslyn.Asserts.Tests/Analyzers/FieldAndPropertyMustBeNamedFooAnalyzer.cs
@@ -0,0 +1,55 @@
+namespace Gu.Roslyn.Asserts.Tests
+{
+    using System.Collections.Immutable;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.CSharp;
+    using Microsoft.CodeAnalysis.Diagnostics;
+
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    internal class FieldAndPropertyMustBeNamedFooAnalyzer : DiagnosticAnalyzer
+    {
+        public const string FieldDiagnosticId = "Field";
+        public const string PropertyDiagnosticId = "Property";
+
+        private static readonly DiagnosticDescriptor FieldDescriptor = new DiagnosticDescriptor(
+            id: FieldDiagnosticId,
+            title: "The field must be named foo.",
+            messageFormat: "Message format.",
+            category: "Category",
+            defaultSeverity: DiagnosticSeverity.Warning,
+            isEnabledByDefault: true);
+
+        private static readonly DiagnosticDescriptor PropertyDescriptor = new DiagnosticDescriptor(
+            id: PropertyDiagnosticId,
+            title: "The Property must be named Foo.",
+            messageFormat: "Message format.",
+            category: "Category",
+            defaultSeverity: DiagnosticSeverity.Warning,
+            isEnabledByDefault: true);
+
+        /// <inheritdoc/>
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(
+            FieldDescriptor,
+            PropertyDescriptor);
+
+        public override void Initialize(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(HandleDeclaration, SyntaxKind.FieldDeclaration, SyntaxKind.PropertyDeclaration);
+        }
+
+        private static void HandleDeclaration(SyntaxNodeAnalysisContext context)
+        {
+            if (context.ContainingSymbol is IFieldSymbol field &&
+                field.Name != "foo")
+            {
+                context.ReportDiagnostic(Diagnostic.Create(FieldDescriptor, context.Node.GetLocation()));
+            }
+
+            if (context.ContainingSymbol is IPropertySymbol property &&
+                property.Name != "foo")
+            {
+                context.ReportDiagnostic(Diagnostic.Create(PropertyDescriptor, context.Node.GetLocation()));
+            }
+        }
+    }
+}
