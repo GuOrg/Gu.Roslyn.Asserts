60237bca9d84d3e6da572862dfba92971632df37
JohanLarsson <johan_gavle1@hotmail.com>
2018-03-22 14:15:08 +0100

    BUGFIX: FindBestMatch + more overloads, fix #49.
    (cherry picked from commit 2a9eada195ce47abd2812ae38780c2a74925d40d)

--- Gu.Roslyn.Asserts/SyntaxNodeExt.cs
+++ Gu.Roslyn.Asserts/SyntaxNodeExt.cs
@@ -35,6 +35,30 @@ namespace Gu.Roslyn.Asserts
         }
 
         /// <summary>
+        /// Find a <see cref="ParameterSyntax"/> that matches <paramref name="signature"/>.
+        /// </summary>
+        public static ParameterSyntax FindParameter(this SyntaxTree tree, string signature)
+        {
+            return tree.FindBestMatch<ParameterSyntax>(signature);
+        }
+
+        /// <summary>
+        /// Find a <see cref="ArgumentSyntax"/> that matches <paramref name="signature"/>.
+        /// </summary>
+        public static ArgumentSyntax FindArgument(this SyntaxTree tree, string signature)
+        {
+            return tree.FindBestMatch<ArgumentSyntax>(signature);
+        }
+
+        /// <summary>
+        /// Find a <see cref="LiteralExpressionSyntax"/> that matches <paramref name="signature"/>.
+        /// </summary>
+        public static LiteralExpressionSyntax FindLiteralExpression(this SyntaxTree tree, string signature)
+        {
+            return tree.FindBestMatch<LiteralExpressionSyntax>(signature);
+        }
+
+        /// <summary>
         /// Find a <see cref="StatementSyntax"/> that matches <paramref name="code"/>.
         /// </summary>
         public static StatementSyntax FindStatement(this SyntaxTree tree, string code)
@@ -61,6 +85,14 @@ namespace Gu.Roslyn.Asserts
         /// <summary>
         /// Find a <see cref="MethodDeclarationSyntax"/> that matches <paramref name="signature"/>.
         /// </summary>
+        public static StructDeclarationSyntax FindStructDeclaration(this SyntaxTree tree, string signature)
+        {
+            return tree.FindBestMatch<StructDeclarationSyntax>(signature);
+        }
+
+        /// <summary>
+        /// Find a <see cref="MethodDeclarationSyntax"/> that matches <paramref name="signature"/>.
+        /// </summary>
         public static FieldDeclarationSyntax FindFieldDeclaration(this SyntaxTree tree, string signature)
         {
             return tree.FindBestMatch<FieldDeclarationSyntax>(signature);
@@ -72,6 +104,62 @@ namespace Gu.Roslyn.Asserts
         public static PropertyDeclarationSyntax FindPropertyDeclaration(this SyntaxTree tree, string signature)
         {
             return tree.FindBestMatch<PropertyDeclarationSyntax>(signature);
+        }
+
+        /// <summary>
+        /// Find a <see cref="IndexerDeclarationSyntax"/> that matches <paramref name="signature"/>.
+        /// </summary>
+        public static IndexerDeclarationSyntax FindIndexerDeclaration(this SyntaxTree tree, string signature)
+        {
+            return tree.FindBestMatch<IndexerDeclarationSyntax>(signature);
+        }
+
+        /// <summary>
+        /// Find a <see cref="EventFieldDeclarationSyntax"/> that matches <paramref name="signature"/>.
+        /// </summary>
+        public static EventFieldDeclarationSyntax FindEventFieldDeclaration(this SyntaxTree tree, string signature)
+        {
+            return tree.FindBestMatch<EventFieldDeclarationSyntax>(signature);
+        }
+
+        /// <summary>
+        /// Find a <see cref="EventDeclarationSyntax"/> that matches <paramref name="signature"/>.
+        /// </summary>
+        public static EventDeclarationSyntax FindEventDeclaration(this SyntaxTree tree, string signature)
+        {
+            return tree.FindBestMatch<EventDeclarationSyntax>(signature);
+        }
+
+        /// <summary>
+        /// Find a <see cref="AccessorDeclarationSyntax"/> that matches <paramref name="signature"/>.
+        /// </summary>
+        public static AccessorDeclarationSyntax FindAccessorDeclaration(this SyntaxTree tree, string signature)
+        {
+            return tree.FindBestMatch<AccessorDeclarationSyntax>(signature);
+        }
+
+        /// <summary>
+        /// Find a <see cref="VariableDeclarationSyntax"/> that matches <paramref name="signature"/>.
+        /// </summary>
+        public static VariableDeclarationSyntax FindVariableDeclaration(this SyntaxTree tree, string signature)
+        {
+            return tree.FindBestMatch<VariableDeclarationSyntax>(signature);
+        }
+
+        /// <summary>
+        /// Find a <see cref="MemberAccessExpressionSyntax"/> that matches <paramref name="signature"/>.
+        /// </summary>
+        public static MemberAccessExpressionSyntax FindMemberAccessExpression(this SyntaxTree tree, string signature)
+        {
+            return tree.FindBestMatch<MemberAccessExpressionSyntax>(signature);
+        }
+
+        /// <summary>
+        /// Find a <see cref="BinaryExpressionSyntax"/> that matches <paramref name="signature"/>.
+        /// </summary>
+        public static BinaryExpressionSyntax FindBinaryExpression(this SyntaxTree tree, string signature)
+        {
+            return tree.FindBestMatch<BinaryExpressionSyntax>(signature);
         }
 
         /// <summary>
@@ -114,39 +202,39 @@ namespace Gu.Roslyn.Asserts
         public static T FindBestMatch<T>(this SyntaxTree tree, string code)
             where T : SyntaxNode
         {
-            SyntaxNode parent = null;
             T best = null;
-            foreach (var node in tree.GetRoot()
-                                     .DescendantNodes()
+            if (tree.TryGetRoot(out var root))
+            {
+                best = FindBestMatchRecursive<T>(root, code);
+            }
+
+            return best ?? throw new InvalidOperationException($"The tree does not contain an {typeof(T).Name} matching the code.");
+        }
+
+        /// <summary>
+        /// Find a <typeparamref name="T"/> that matches <paramref name="code"/>.
+        /// </summary>
+        /// <typeparam name="T">The type of the node to find.</typeparam>
+        public static T FindBestMatch<T>(this SyntaxNode root, string code)
+            where T : SyntaxNode
+        {
+            return FindBestMatchRecursive<T>(root, code) ?? throw new InvalidOperationException($"The tree does not contain an {typeof(T).Name} matching the code.");
+        }
+
+        private static T FindBestMatchRecursive<T>(SyntaxNode root, string code)
+            where T : SyntaxNode
+        {
+            foreach (var node in root.DescendantNodes()
                                      .OfType<T>())
             {
-                var statementSyntax = node.FirstAncestorOrSelf<StatementSyntax>();
-                if (statementSyntax?.ToFullString().Contains(code) == true)
+                if (node.ToFullString().Contains(code) ||
+                    node.FirstAncestorOrSelf<StatementSyntax>()?.ToFullString().Contains(code) == true)
                 {
-                    if (parent == null || statementSyntax.Span.Length < parent.Span.Length)
-                    {
-                        parent = statementSyntax;
-                        best = node;
-                    }
-                }
-
-                var member = node.FirstAncestorOrSelf<MemberDeclarationSyntax>();
-                if (member?.ToFullString().Contains(code) == true)
-                {
-                    if (parent == null || member.Span.Length < parent.Span.Length)
-                    {
-                        parent = member;
-                        best = node;
-                    }
+                    return FindBestMatchRecursive<T>(node, code) ?? node;
                 }
             }
 
-            if (best == null)
-            {
-                throw new InvalidOperationException($"The tree does not contain an {typeof(T).Name} matching the code.");
-            }
-
-            return best;
+            return null;
         }
     }
 }
