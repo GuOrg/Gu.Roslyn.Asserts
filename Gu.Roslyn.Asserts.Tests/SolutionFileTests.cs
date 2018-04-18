3e63f3c21fa4f27988a1cb4f3a4d57f2bf852412
JohanLarsson <johan_gavle1@hotmail.com>
2018-04-18 15:52:43 +0200

    Split tests.

--- Gu.Roslyn.Asserts.Tests/SolutionFileTests.cs
+++ Gu.Roslyn.Asserts.Tests/SolutionFileTests.cs
@@ -1,15 +1,22 @@
-﻿namespace Gu.Roslyn.Asserts.Tests
+namespace Gu.Roslyn.Asserts.Tests
 {
     using NUnit.Framework;
 
     public class SolutionFileTests
     {
         [Test]
-        public void FindSolutionFile()
+        public void TryFind()
         {
             Assert.AreEqual(true, SolutionFile.TryFind("Gu.Roslyn.Asserts.sln", out var sln));
             Assert.AreEqual("Gu.Roslyn.Asserts.sln", sln.Name);
             sln = SolutionFile.Find("Gu.Roslyn.Asserts.sln");
+            Assert.AreEqual("Gu.Roslyn.Asserts.sln", sln.Name);
+        }
+
+        [Test]
+        public void Find()
+        {
+            var sln = SolutionFile.Find("Gu.Roslyn.Asserts.sln");
             Assert.AreEqual("Gu.Roslyn.Asserts.sln", sln.Name);
         }
     }
