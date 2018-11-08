c044e8d4654884171ce0f21f090042dca277134d
JohanLarsson <johan_gavle1@hotmail.com>
2018-11-08 14:38:12 +0100

    More tests & asserts.

--- Gu.Roslyn.Asserts.Tests/MetadataReferences/ReferenceAssemblyTests.cs
+++ Gu.Roslyn.Asserts.Tests/MetadataReferences/ReferenceAssemblyTests.cs
@@ -2,16 +2,23 @@ namespace Gu.Roslyn.Asserts.Tests.MetadataReferences
 {
     using System;
     using System.IO;
+    using Microsoft.CodeAnalysis;
     using NUnit.Framework;
 
     public class ReferenceAssemblyTests
     {
         [TestCase(typeof(int))]
+        [TestCase(typeof(System.Diagnostics.Debug))]
         public void TryGet(Type type)
         {
             Assert.AreEqual(true, ReferenceAssembly.TryGet(type.Assembly, out var metadataReference));
+            StringAssert.Contains("Reference Assemblies", ((PortableExecutableReference)metadataReference).FilePath);
+
             Assert.AreEqual(true, ReferenceAssembly.TryGet(type.Assembly.Location, out metadataReference));
+            StringAssert.Contains("Reference Assemblies", ((PortableExecutableReference)metadataReference).FilePath);
+
             Assert.AreEqual(true, ReferenceAssembly.TryGet(Path.GetFileNameWithoutExtension(type.Assembly.Location), out metadataReference));
+            StringAssert.Contains("Reference Assemblies", ((PortableExecutableReference)metadataReference).FilePath);
         }
     }
 }
