dc4e786decfc8fde804a918ae07c5ca187f00fae
JohanLarsson <johan_gavle1@hotmail.com>
2019-07-12 17:15:01 +0200

    Trim placeholders.
    Trimming the code to a minimum as they were annoying to debug.

--- Gu.Roslyn.Asserts.Analyzers.Tests/Code.cs
+++ Gu.Roslyn.Asserts.Analyzers.Tests/Code.cs
@@ -7,35 +7,15 @@ namespace RoslynSandbox
 {
     using System.Collections.Immutable;
     using Microsoft.CodeAnalysis;
-    using Microsoft.CodeAnalysis.CSharp;
     using Microsoft.CodeAnalysis.Diagnostics;
 
-    [DiagnosticAnalyzer(LanguageNames.CSharp)]
     internal class PlaceholderAnalyzer : DiagnosticAnalyzer
     {
-        internal const string DiagnosticId = ""NoError"";
-
-        internal static readonly DiagnosticDescriptor Descriptor = new DiagnosticDescriptor(
-            id: DiagnosticId,
-            title: ""This analyzer never reports an error."",
-            messageFormat: ""Message format."",
-            category: ""Category"",
-            defaultSeverity: DiagnosticSeverity.Warning,
-            isEnabledByDefault: true);
-
         /// <inheritdoc/>
-        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; } = ImmutableArray.Create(Descriptor);
+        public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics { get; }
 
         public override void Initialize(AnalysisContext context)
         {
-            if (context == null)
-            {
-                throw new System.ArgumentNullException(nameof(context));
-            }
-
-            context.EnableConcurrentExecution();
-            context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
-            context.RegisterSyntaxNodeAction(Handle, SyntaxKind.IdentifierName);
         }
 
         private static void Handle(SyntaxNodeAnalysisContext context)
@@ -49,17 +29,27 @@ namespace RoslynSandbox
 {
     using System.Collections.Immutable;
     using System.Threading.Tasks;
-    using Microsoft.CodeAnalysis;
     using Microsoft.CodeAnalysis.CodeFixes;
 
-    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(PlaceholderFix))]
-    internal class PlaceholderFix : CodeFixProvider
+    public class PlaceholderFix : CodeFixProvider
     {
-        public override ImmutableArray<string> FixableDiagnosticIds { get; } = ImmutableArray.Create(PlaceholderAnalyzer.DiagnosticId);
+        public override ImmutableArray<string> FixableDiagnosticIds { get; }
 
         public override FixAllProvider GetFixAllProvider() => null;
 
-        public override Task RegisterCodeFixesAsync(CodeFixContext context) => Task.FromResult(true);
+        public override Task RegisterCodeFixesAsync(CodeFixContext context) => Task.CompletedTask;
+    }
+}";
+
+        public const string PlaceholderRefactoring = @"
+namespace RoslynSandbox
+{
+    using System.Threading.Tasks;
+    using Microsoft.CodeAnalysis.CodeRefactorings;
+
+    public class PlaceholderRefactoring : CodeRefactoringProvider
+    {
+        public override Task ComputeRefactoringsAsync(CodeRefactoringContext context) => Task.CompletedTask;
     }
 }";
     }
