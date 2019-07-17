05decfb160f75b085284f1d4700a123325466d2b
JohanLarsson <johan_gavle1@hotmail.com>
2019-07-17 13:40:21 +0200

    Move explicit tests to issue #87

--- Gu.Roslyn.Asserts.Tests.Net472WithAttributes/RoslynAssertTests.ProjectAndSln.cs
+++ Gu.Roslyn.Asserts.Tests.Net472WithAttributes/RoslynAssertTests.ProjectAndSln.cs
@@ -41,29 +41,5 @@ namespace Gu.Roslyn.Asserts.Tests.Net472WithAttributes
             RoslynAssert.Valid(analyzer, descriptor, code);
             RoslynAssert.Valid(analyzer, code, compilationOptions: CodeFactory.DefaultCompilationOptions(analyzer, descriptor, null));
         }
-
-        [Explicit("Need to solve signing and InternalsVisibleTo")]
-        [Test]
-        public void SolutionFileNopAnalyzer()
-        {
-            var code = SolutionFile.Find("Gu.Roslyn.Asserts.sln");
-            var analyzer = new NopAnalyzer();
-            RoslynAssert.Valid(typeof(NopAnalyzer), code);
-            RoslynAssert.Valid(analyzer, code);
-
-            var descriptor = NopAnalyzer.Descriptor;
-            RoslynAssert.Valid(typeof(NopAnalyzer), descriptor, code);
-            RoslynAssert.Valid(analyzer, descriptor, code);
-        }
-
-        [Explicit("Need to solve signing and InternalsVisibleTo")]
-        [Test]
-        public void SolutionNopAnalyzer()
-        {
-            var sln = SolutionFile.Find("Gu.Roslyn.Asserts.sln");
-            var solution = CodeFactory.CreateSolution(sln, new[] { new NopAnalyzer() }, RoslynAssert.MetadataReferences);
-            RoslynAssert.Valid(typeof(NopAnalyzer), solution);
-            RoslynAssert.Valid(new NopAnalyzer(), solution);
-        }
     }
 }
