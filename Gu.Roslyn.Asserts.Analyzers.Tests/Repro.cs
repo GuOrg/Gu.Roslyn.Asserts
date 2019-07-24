0b4b97402b1718ad0aeb411304fd24eecc31ae56
Johan Larsson <johan_gavle1@hotmail.com>
2019-07-24 22:25:26 +0200

    ValidWithAllAnalyzers

---
+++ Gu.Roslyn.Asserts.Analyzers.Tests/Repro.cs
@@ -0,0 +1,34 @@
+namespace Gu.Roslyn.Asserts.Analyzers.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using NUnit.Framework;
+
+#pragma warning disable GURA04, GURA06
+    [Explicit("Only for digging out test cases.")]
+    public static class Repro
+    {
+        // ReSharper disable once UnusedMember.Local
+        private static readonly IReadOnlyList<DiagnosticAnalyzer> AllAnalyzers =
+            typeof(Descriptors).Assembly.GetTypes()
+                               .Where(typeof(DiagnosticAnalyzer).IsAssignableFrom)
+                               .Select(t => (DiagnosticAnalyzer)Activator.CreateInstance(t))
+                               .ToArray();
+
+        private static readonly Solution Solution = CodeFactory.CreateSolution(
+            new FileInfo(@"C:\Git\_DotNetAnalyzers\ReflectionAnalyzers\ReflectionAnalyzers.sln"),
+            AllAnalyzers,
+            MetadataReferences.FromAttributes());
+
+        [TestCaseSource(nameof(AllAnalyzers))]
+        public static void Run(DiagnosticAnalyzer analyzer)
+        {
+            var diagnostics = Analyze.GetDiagnostics(Solution, analyzer);
+            RoslynAssert.NoDiagnostics(diagnostics);
+        }
+    }
+}
---
+++ Gu.Roslyn.Asserts.Analyzers.Tests/ValidWithAllAnalyzers.cs
@@ -0,0 +1,45 @@
+namespace Gu.Roslyn.Asserts.Analyzers.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Microsoft.CodeAnalysis;
+    using Microsoft.CodeAnalysis.Diagnostics;
+    using NUnit.Framework;
+
+#pragma warning disable GURA04, GURA06
+    public static class ValidWithAllAnalyzers
+    {
+        private static readonly IReadOnlyList<DiagnosticAnalyzer> AllAnalyzers = typeof(Descriptors)
+                                                                                 .Assembly.GetTypes()
+                                                                                 .Where(typeof(DiagnosticAnalyzer).IsAssignableFrom)
+                                                                                 .Select(t => (DiagnosticAnalyzer)Activator.CreateInstance(t))
+                                                                                 .ToArray();
+
+        private static readonly Solution AnalyzersTests = CodeFactory.CreateSolution(
+            ProjectFile.Find("Gu.Roslyn.Asserts.Analyzers.Tests.csproj"),
+            MetadataReferences.FromAttributes());
+
+        [Test]
+        public static void NotEmpty()
+        {
+            CollectionAssert.IsNotEmpty(AllAnalyzers);
+            Assert.Pass($"Count: {AllAnalyzers.Count}");
+        }
+
+        [TestCaseSource(nameof(AllAnalyzers))]
+        public static void AnalyzersTestsProject(DiagnosticAnalyzer analyzer)
+        {
+            switch (analyzer)
+            {
+                case InvocationAnalyzer _:
+                case MethodDeclarationAnalyzer _:
+                    _ = Analyze.GetDiagnostics(analyzer, AnalyzersTests);
+                    break;
+                default:
+                    RoslynAssert.Valid(analyzer, AnalyzersTests);
+                    break;
+            }
+        }
+    }
+}
