8ab2195ee7b2baafb75449c3f64b4cb57306f20d
JohanLarsson <johan_gavle1@hotmail.com>
2019-08-04 16:11:50 +0200

    Try nuget, everything is broken.

--- Gu.Roslyn.Asserts.Analyzers/AnalyzerCategory.cs
+++ Gu.Roslyn.Asserts.Analyzers/AnalyzerCategory.cs
@@ -2,8 +2,8 @@ namespace Gu.Roslyn.Asserts.Analyzers
 {
     internal class AnalyzerCategory
     {
-        public static readonly string Ocd = nameof(Ocd);
+        internal static readonly string Ocd = nameof(Ocd);
 
-        public static readonly string Correctness = nameof(Correctness);
+        internal static readonly string Correctness = nameof(Correctness);
     }
 }
