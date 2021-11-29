151009dd3dc9cdd75903aa85d09a0d7a729dff24
JohanLarsson <johan_gavle1@hotmail.com>
2021-11-29 17:13:51 +0100

    Fix tests.

--- Gu.Roslyn.Asserts/CodeFactory.cs
+++ Gu.Roslyn.Asserts/CodeFactory.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Collections.Immutable;
     using System.ComponentModel;
     using System.Diagnostics.CodeAnalysis;
     using System.IO;
