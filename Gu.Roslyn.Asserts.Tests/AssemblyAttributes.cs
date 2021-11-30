8100343e9dd340bf15d8b63f9f79bb9db3686c8d
JohanLarsson <johan_gavle1@hotmail.com>
2021-11-30 13:13:42 +0100

    Use settings.

--- Gu.Roslyn.Asserts.Tests/AssemblyAttributes.cs
+++ Gu.Roslyn.Asserts.Tests/AssemblyAttributes.cs
@@ -1,6 +1,3 @@
 ﻿using System;
-using Gu.Roslyn.Asserts;
 
 [assembly: CLSCompliant(false)]
-[assembly: MetadataReference(typeof(object), new[] { "global", "mscorlib" })]
-[assembly: MetadataReference(typeof(System.Diagnostics.Debug), new[] { "global", "System" })]
