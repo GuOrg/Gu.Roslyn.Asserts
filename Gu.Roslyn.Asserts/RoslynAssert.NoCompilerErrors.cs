9271a0f49ad1b18b29b9dcb5d7faa80799954fd0
JohanLarsson <johan_gavle1@hotmail.com>
2021-12-01 13:57:37 +0100

    Fix warning

--- Gu.Roslyn.Asserts/RoslynAssert.NoCompilerErrors.cs
+++ Gu.Roslyn.Asserts/RoslynAssert.NoCompilerErrors.cs
@@ -1,7 +1,6 @@
 ﻿namespace Gu.Roslyn.Asserts
 {
     using System.Collections.Generic;
-    using System.Collections.Immutable;
     using System.Globalization;
     using System.Linq;
     using Gu.Roslyn.Asserts.Internals;
